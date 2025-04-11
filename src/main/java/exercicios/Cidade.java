70b4413bb16469e97c0c89a8e2bd21e1c2f58aca
Wahbene H. <wahbene15@gmail.com>
2025-04-11 16:15:26 -0300

    Criação da classe "Cidade", sendo uma classe mutável

---
+++ src/main/java/exercicios/Cidade.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+// Classe "Cidade" mutável
+public class Cidade {
+
+    private String nome;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
